4b20b7943bd03771d57cd5c753195b12e337a21a
johndoekim <amok0316@gmail.com>
2024-02-23 06:19:55 +0900

    편지봉투 80%

--- src/component/Mission/MissonPaper.tsx
+++ src/component/Mission/MissonPaper.tsx
@@ -16,37 +16,17 @@ export const MissionPaper: React.FC<MissionPaperProps> = ({
 }) => {
   return (
     <>
-      <View className="bg-HoneyYellow w-80 h-96">
-        <View className="items-center mt-2 mx-2 border-x-2 border-t-2 border-Blueberry">
-          <Text className="text-Blueberry text-3xl font-extrabold">
-            R A D I O G R A M
-          </Text>
-          <Text className="text-Blueberry font-semibold">
-            STANDARD AIR LINES INC.
-          </Text>
+      <View className="bg-white w-[345px] h-[400px] rounded border-[1px] border-[#DBD2B5]">
+        <View className="ml-8 mt-6">
+          <Text className="text-[16px]">발신자</Text>
+          <Text className="text-[16px]">미션 기한</Text>
+          <Text className="text-[16px]">수취인</Text>
         </View>
-        <View className="justify-between flex-row mx-2 border-t-2 border-b-2 border-Blueberry">
-          <View className="w-24 h-14 border-x-2 border-Blueberry">
-            <Text className="font-light text-xs mx-1 mt-1 italic text-center text-Blueberry">
-              RADIOGRAMS TO ALL PARTS of the WORLD
-            </Text>
-          </View>
-          <Ionicons name="earth" size={54} color="#2C333E" />
-          <View className="w-24 h-14 border-x-2 border-Blueberry">
-            <Text className="font-light text-xs mx-1 mt-1 italic text-center text-Blueberry">
-              SHIP TO SHORE SHORE TO SHIP
-            </Text>
-          </View>
-        </View>
-        <View className="mx-2 border-x-2 border-b-2 h-64 border-Blueberry">
-          <View className="mx-2 mt-2">
-            <Text className="text-xl text-Blueberry">TO {nickname}</Text>
-            <Text className="mt-4 text-lg text-Blueberry">{title}</Text>
-            <Text className="mt-2 text-Blueberry">{content}</Text>
-            <Text className="text-lg mt-10 text-Blueberry">
-              제한 시간 : {time}
-            </Text>
-          </View>
+        <View className="items-center justify-center mx-12 mt-20">
+          <Text className="text-xl text-center">
+            오늘의 미션은 맛있는 음식 요리하기 입니다. 내 손으로 음식을 만들어
+            가족이나 친구와 맛있는 식사를 즐기세요
+          </Text>
         </View>
       </View>
     </>
--- src/screen/Mission/GetMissionScreen.tsx
+++ src/screen/Mission/GetMissionScreen.tsx
@@ -2,7 +2,8 @@ import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import {Avatar} from '../../component/Common/Avatar';
 import {MissionPaper} from '../../component/Mission/MissonPaper';
 import Toast from 'react-native-toast-message';
-
+import Check from '../../../assets/Icons/Mission/Check.svg';
+import X from '../../../assets/Icons/Mission/X.svg';
 export const GetMissionScreen: React.FC = () => {
   const dummyData = {
     nickname: 'JoneDoe',
@@ -41,26 +42,33 @@ export const GetMissionScreen: React.FC = () => {
             content={dummyData.content}
             time={dummyData.time}
           />
-          <View className="mt-5">
+          <View className="mt-12">
             <TouchableOpacity
-              className="bg-Quicksand w-40 rounded-xl items-center"
+              className="bg-[#58533D] w-[345px] h-[60px] rounded-[8px] items-center flex-row justify-center"
               onPress={showToast}>
+              <Check />
               <Text
-                className="text-Pristine
-            text-3xl 
-            font-medium
-            my-1.5">
+                className="text-[#FFF8DC]
+            text-[20px]
+            font-bold
+            my-4
+            ml-2
+            ">
                 미션 수락
               </Text>
             </TouchableOpacity>
             <TouchableOpacity
-              className="bg-Quicksand mt-3 w-40 rounded-xl items-center"
-              onPress={showToast2}>
+              className="bg-MainBg mt-2 w-[345px] h-[60px] rounded-[8px] border-2 border-[#58533D] items-center flex-row justify-center"
+              onPress={showToast}>
+              <X />
               <Text
-                className="text-Pristine
-            text-3xl 
-            font-medium
-            my-1.5">
+                className="text-[#FFF8DC]
+            text-[20px]
+            text-[#58533D]
+            font-bold
+            my-4
+            ml-2
+            ">
                 미션 거절
               </Text>
             </TouchableOpacity>
